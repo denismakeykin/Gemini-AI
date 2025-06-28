086f4bf5fb6744aaff4e2dc449ab37e7106058f1
denismakeykin <denismakeykin@gmail.com>
2025-06-28 17:53:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -535,3 +535,4 @@ if __name__ == '__main__':
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
         logger.info("Приложение остановлено пользователем.")
+--- END OF FILE main.py.txt ---
